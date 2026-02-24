db2e1948d3a4f132444c69f1df1cd6e70ccb5be2
조하은 <jhe93@naver.com>
2026-02-25 04:20:02 +0900

    fix: 문구 수정

--- src/routes/random.tsx
+++ src/routes/random.tsx
@@ -1,9 +1,9 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/random')({
-  component: RouteComponent,
-})
+    component: RouteComponent,
+});
 
 function RouteComponent() {
-  return <div>Hello "/random"!</div>
+    return <div className="w-full h-full flex items-center justify-center text-gray-2">곧 추가될 페이지입니다 ...</div>;
 }
