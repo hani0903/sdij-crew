b8e1e4d9d2c2cce79de2c87df683dc1054854794
조하은 <jhe93@naver.com>
2026-02-25 06:21:02 +0900

    feat: 로딩스피너 구현

---
+++ src/components/ui/CircularLoadingSpinner.tsx
@@ -0,0 +1,5 @@
+export default function CircularLoadingSpinner() {
+    return (
+        <div className="h-12 md:h-[120px] md:w-[120px] w-12 animate-spin rounded-full border-[6px] border-gray-200 mobile:border-[16px] border-t-point" />
+    );
+}
